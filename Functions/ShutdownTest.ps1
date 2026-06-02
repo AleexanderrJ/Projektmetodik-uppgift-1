c0dd5c74015ef1d917e9fdbbd1df15cbcc8eff05
alisulaiman-debug <ali.sulaiman@student.ksgyf.se>
2026-06-02 12:12:18 +0200

    Update ShutdownTest.ps1

--- Functions/ShutdownTest.ps1
+++ Functions/ShutdownTest.ps1
@@ -13,21 +13,3 @@ function Start-Shutdown {
     shutdown /s /t 10
 
 }
-
-$Choice = Read-Host "Välj: 1 = Viloläge | 2 = Avstängning"
-
-if ($Choice -eq "1") {
-
-    Start-Hibernate
-
-}
-elseif ($Choice -eq "2") {
-
-    Start-Shutdown
-
-}
-else {
-
-    Write-Host "Felaktigt val."
-
-}
