0dcd4919ae4fa50708dde5d05955d3cc4233c4a4
alisulaiman-debug <ali.sulaiman@student.ksgyf.se>
2026-06-02 12:03:47 +0200

    Update GreenIT.psm1

--- Functions/GreenIT.psm1
+++ Functions/GreenIT.psm1
@@ -1,1 +1,22 @@
+. $PSScriptRoot\ShutdownLogger.ps1
+. $PSScriptRoot\ShutdownTest.ps1
 
+function Start-GreenIT {
+
+    Write-Host "Loggar datorinformation..."
+    Write-Shutdownlog -ComputerName $env:COMPUTERNAME
+
+    $choice = Read-Host "1 = Viloläge | 2 = Avstängning"
+
+    if ($choice -eq "1") {
+        Start-Hibernate
+    }
+    elseif ($choice -eq "2") {
+        Start-Shutdown
+    }
+    else {
+        Write-Host "Felaktigt val."
+    }
+}
+
+Export-ModuleMember -Function Start-GreenIT
