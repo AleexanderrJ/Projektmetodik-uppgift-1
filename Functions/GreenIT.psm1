7408558fb329cfc90ef9752097f9b741c05dcf65
alisulaiman-debug <ali.sulaiman@student.ksgyf.se>
2026-06-03 13:20:59 +0200

    Update GreenIT.psm1

--- Functions/GreenIT.psm1
+++ Functions/GreenIT.psm1
@@ -1,44 +1,44 @@
-# Importerar funktionerna från de andra scriptfilerna
+# Importera funktioner
 . $PSScriptRoot\ShutdownLogger.ps1
 . $PSScriptRoot\ShutdownTest.ps1
 . $PSScriptRoot\aktivaKlienter.ps1
+. $PSScriptRoot\Inactiveclients.ps1
 
-# Huvudfunktion som styr Green IT-flödet
 function Start-GreenIT {
 
-    # Söker efter aktiva klienter i nätverket
     Write-Host "1. Söker aktiva klienter..."
     $clients = Get-NetworkClients
 
     Write-Host ""
 
-    # Loggar datorinformation och tidpunkt
-    Write-Host "2. Loggar datorinformation..."
+    Write-Host "2. Söker inaktiva klienter..."
+    $inactiveClients = Find-InactiveClients
 
-    # Hämtar logginformation för aktuell dator
+    Write-Host ""
+
+    # Logga serverinformation
     $logg = Write-Shutdownlog -ComputerName $env:COMPUTERNAME
 
-    # Skriver ut logginformation till terminalen
     Write-Host "Datornamn: $($logg.ComputerName)"
     Write-Host "Användare: $($logg.UserName)"
     Write-Host "Tidpunkt: $($logg.TimeStamp)"
 
     Write-Host ""
 
-    # Bestämmer automatiskt åtgärd beroende på aktiva klienter
-    if ($clients.Count -gt 0) {
+    # Om inaktiva klienter hittas
+    if ($inactiveClients.Count -gt 0) {
 
-        # Om aktiva klienter finns → sätt datorn i viloläge
-        Write-Host "Aktiva klienter hittades. Datorn sätts i viloläge."
-        Start-Hibernate
+        Write-Host "$($inactiveClients.Count) inaktiva klienter hittades."
+        Write-Host "Shutdown startas..."
+
+        Start-Shutdown
+
     }
     else {
 
-        # Om inga klienter hittas → stäng av datorn
-        Write-Host "Inga aktiva klienter hittades. Datorn stängs av."
-        Start-Shutdown
+        Write-Host "Inga inaktiva klienter hittades."
+
     }
 }
 
-# Exporterar funktionen så att den kan användas via Import-Module
 Export-ModuleMember -Function Start-GreenIT
